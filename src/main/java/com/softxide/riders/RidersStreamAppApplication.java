020ce8aa61957cc78336fd224f1dc5237ae3c44f
Jimmy, Divyanshu <divyanshu.jimmy@rakuten.com>
2019-10-25 11:40:56 +0800

    first commit

---
+++ src/main/java/com/softxide/riders/RidersStreamAppApplication.java
@@ -0,0 +1,13 @@
+package com.softxide.riders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidersStreamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidersStreamAppApplication.class, args);
+	}
+
+}
